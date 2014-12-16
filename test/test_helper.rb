5879f3ba24871d5f92a118ea5341d5ee7c9b293c
Will Fisher <will@teknofire.net>
2014-12-16 10:06:22 -0900

    resolving warnings due to requires

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,7 @@
+require 'rubygems'
+require 'bundler'
+Bundler.setup(:default, :test)
 require 'shiplogs/parser'
 require 'minitest/unit'
 require 'minitest/autorun'
-require 'minitest/pride'
+# require 'minitest/pride'
