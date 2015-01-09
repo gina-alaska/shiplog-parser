1b04d8859cc300e0e4f0bd0c094f6271a493ecec
Will Fisher <will@teknofire.net>
2015-01-09 10:33:49 -0900

    adding support for second possible format for shiplogs

--- lib/shiplogs/parser/html.rb
+++ lib/shiplogs/parser/html.rb
@@ -2,27 +2,32 @@ module Shiplogs
   module Parser
     class Html
       attr_accessor :url
-      
+
       def initialize(url)
         @url = url
         @pages = []
+        @content_containers = ['div#content', 'td#logs']
       end
-      
+
       def document
         @document ||= Nokogiri::HTML(open(@url))
       end
-      
+
       def content
-        document.css('div#content').first.children
+        while @content.nil?
+          el = @content_containers.shift
+          @content = document.css(el).first
+        end
+        @content.children
       end
-      
+
       def pages
         if @pages.empty?
           previous_node = nil
           current_page = nil
           content.css('p').each do |node|
             next if node.nil?
-            
+
             if title_node?(node)
               if title_node?(previous_node)
                 #found second title for current page
@@ -38,35 +43,35 @@ module Shiplogs
             previous_node = node
           end
         end
-        
+
         @pages
       end
-      
+
       protected
-      
+
       def add_page(page)
         return if page.nil?
-        
+
         previous = @pages.last
-        
+
         #check to see if the previous page has the same title as the current page
         if !previous.nil? and previous.title == page.title
           previous.content << "---"
           previous.content += page.content
         else
-          @pages << page          
+          @pages << page
         end
       end
-      
+
       def title_node?(node)
         return false if node.nil?
         node[:class] == 'center' and title_content(node).length > 0
       end
-      
+
       def title_content(node)
         node.content.gsub("\n", " ").strip
       end
-      
+
       def page_content(node)
         node.content.gsub("\n", " ").strip + "\n"
       end
--- test/shiplogs_parser_html_test.rb
+++ test/shiplogs_parser_html_test.rb
@@ -6,38 +6,44 @@ class ShiplogsParserHtmlTest < MiniTest::Unit::TestCase
     @url = File.expand_path('../fixtures/USS_Bear_1884_log.html', __FILE__)
     @log = Shiplogs::Parser::Html.new(@url)
   end
-  
+
   def test_responds_to_url
     assert_equal @url, @log.url
   end
-  
+
   def test_should_return_html_document
     assert_instance_of Nokogiri::HTML::Document, @log.document
   end
-  
+
   def test_should_return_log_content
     assert !@log.content.nil?
   end
-  
+
   def test_should_find_pages
     assert_equal 227, @log.pages.count
-  end  
-  
+  end
+
   def test_should_pull_out_page_title
     assert_equal 'LOG BOOK – MARCH 17TH 1884 TO SEPTEMBER 22ND 1884', @log.pages[0].title
   end
-  
+
   def test_should_pull_out_page_content
     assert @log.pages[0].content.length > 0, "Page content was empty"
   end
-  
+
   def test_fourth_page_should_be_log_page
     assert_equal :log_page, @log.pages[3].type
   end
-  
+
   def test_duplicate_page_titles
     url = File.expand_path('../fixtures/USRC_Bear-1890-1891.htm', __FILE__)
     log = Shiplogs::Parser::Html.new(url)
     assert log.pages[4].title != log.pages[5].title, "Found pages with duplicate titles"
+  end
+
+  def test_alternate_log_page_format
+    url = File.expand_path("../fixtures/OWShips-WW1-18-HMS_Acacia.htm", __FILE__)
+    log = Shiplogs::Parser::Html.new(url)
+    assert log.pages.count > 0, "Did not find any pages in alternate log format"
   end
 end
